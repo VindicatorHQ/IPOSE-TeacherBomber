404721eb6886a7f00c2a1429754611dd65f1bab6
VindicatorHQ <minecraftgamer725@gmail.com>
2022-04-07 10:21:24 +0200

    Clean up ignored files

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.ward_cunningham_38.teacherbomber {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.ward_cunningham_38.teacherbomber to javafx.fxml;
-    exports com.ward_cunningham_38.teacherbomber;
-}
